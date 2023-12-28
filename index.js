ba151606480e99e22b69f97a713cee800d76dfa2
ryosoro <154116630+ryosoro@users.noreply.github.com>
2023-12-28 11:34:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const cameraHeight = 400;
 const cameraInit = () => {
     const video = document.getElementById("camera");
 
-    const cameraSetting = {
-        audio: false,
-        video: true, 
-    }
+    const constraints = {
+  audio: true,
+  video: { width: 1280, height: 720 },
+};
 
-    navigator.mediaDevices.getUserMedia(cameraSetting)
-        .then((mediaStream) => {
-            video.srcObject = mediaStream;
-        })
-        .catch((err) => {
-            console.log(err.toString());
-        });
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then((mediaStream) => {
+    const video = document.querySelector("video");
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  })
+  .catch((err) => {
+    // 最後に常にエラーをチェックする
+    console.error(`${err.name}: ${err.message}`);
+  });
 }
